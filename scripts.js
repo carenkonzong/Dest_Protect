c89a808208e9c9e6ef638147beee2b01d9864c53
Edwin Tsafack Nkonzong <tsafackedwin@gmail.com>
2025-07-14 06:40:02 -0400

    Added form submission fonctionality

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ function displaymenu() {
 
 const hamburger = document.getElementById("hamburgerl");
 hamburger.addEventListener("click", displaymenu);
+
+document.getElementById("contactform").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const form = document.getElementById("contactform");
+
+  emailjs.sendForm("service_0mdpjz9", "template_dl8a11r", this).then(
+    () => {
+      alert("le formulaire a été soumis avec succès!");
+      form.reset();
+    },
+    (error) => {
+      alert("Failed to send message: " + error);
+    }
+  );
+});
